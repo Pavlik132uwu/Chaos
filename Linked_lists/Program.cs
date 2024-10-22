ae782cee840f92a63670a46f8c233135b44054ce
Pavlik132uwu <pavel.francl@seznam.cz>
2024-10-22 19:39:54 +0200

    Mám rád chleba

---
+++ Linked_lists/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Linked_lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
