8e7c4cc4f56010f096e05af2e55cb1f87ee6af33
Pavlik132uwu <pavel.francl@seznam.cz>
2025-03-26 15:57:17 +0100

    moc jsem toho nepřidal
    přejmenovat na moje

--- connect/Program.cs
+++ connect/Program.cs
@@ -10,22 +10,60 @@ namespace connect
     {
         static void Main(string[] args)
         {
-            int[,] board = new int[6, 7]{
-                { 0, 0, 0, 0, 1, 0, 0 },
-                { 0, 0, 0, 1, 2, 0, 0 },
-                { 0, 0, 1, 1, 1, 0, 0 },
-                { 0, 1, 1, 1, 2, 2, 2 },
-                { 1, 1, 2, 1, 2, 2, 2 },
-                { 1, 2, 2, 1, 2, 2, 2 }};
-            int[] position = { 0, 4 };
-            // pozice řádek s indexem 0
-            // pozice sloupec s indexem 4
-            if (CheckWin(board, 5, 1, position)) //nutno změnit tu 3 proměnou za hráče (1 nebo 2)
-            {
-                Console.WriteLine("Vyhrál jsi");
-            }
+            //int[,] board = new int[6, 7]{
+            //    { 0, 0, 0, 0, 1, 0, 0 },
+            //    { 0, 0, 0, 1, 2, 0, 0 },
+            //    { 0, 0, 1, 1, 1, 0, 0 },
+            //    { 0, 1, 1, 1, 2, 2, 2 },
+            //    { 1, 1, 2, 1, 2, 2, 2 },
+            //    { 1, 2, 2, 1, 2, 2, 2 }};
+            //int[] position = { 0, 4 };
+            //// pozice řádek s indexem 0
+            //// pozice sloupec s indexem 4
+            //if (CheckWin(board, 5, 1, position)) //nutno změnit tu 3 proměnou za hráče (1 nebo 2)
+            //{
+            //    Console.WriteLine("Vyhrál jsi");
+            //}
+
+            int pocetHrac = 2;
+            int neededToWin = 5;
+            int vyska = 7;
+            int sirka = 7;
+            Hra hra1 = new Hra(vyska, sirka, neededToWin, pocetHrac)
 
         }
+    }
+
+    class Hra
+    {
+        // toto dát s mými proměnými a tak
+
+        //public Hra(int pocetVyhernichZetonu, int sirkaPole, int vyskaPole, int pocetHracu)
+        //{
+        //    this.pocetVyhernichZetonu = pocetVyhernichZetonu;
+        //    hraciPole = new int[sirkaPole, vyskaPole];
+        //    hraci = new Hrac[pocetHracu];
+        //}
+
+        //int pocetVyhernichZetonu; // datová položka
+        //int[,] hraciPole;
+
+        //Hrac[] hraci;
+        //public Hrac Play()
+        //{
+        //    Hrac hrac = new Hrac();
+        //    Position startPosition = new Position();
+        //    Console.WriteLine("Na řadě je hráč", hrac.Jmeno);
+        //    startPosition.Row = 0;
+        //    startPosition.Column = 0;
+
+
+        //    // Tah
+        //    // Check
+        //    //střídání hračů
+        //}
+
+
         public static bool CheckWin(int[,] board, int neededToWin, int hrac, int[] soucasnaPozice)
         {
             int rows = board.GetLength(0);
@@ -33,10 +71,10 @@ namespace connect
             int x = soucasnaPozice[0];
             int y = soucasnaPozice[1];
 
-            // Kontrola řádku
+            // Kontrola řádku -
             if (CheckDirection(board, neededToWin, hrac, x, y, 0, 1)) return true;
 
-            // Kontrola sloupce
+            // Kontrola sloupce | 
             if (CheckDirection(board, neededToWin, hrac, x, y, 1, 0)) return true;
 
             // Kontrola diagonály \
@@ -77,4 +115,16 @@ namespace connect
             return count >= neededToWin; //tohle mi bude dávat true nebo false
         }
     }
+    //taky předělat na moje
+
+    //class Hrac
+    //{
+    //    public string Jmeno;
+    //    public string Symbol;
+    //}
+    //struct Position
+    //{
+    //    public int Row;
+    //    public int Column;
+    //}
 }
